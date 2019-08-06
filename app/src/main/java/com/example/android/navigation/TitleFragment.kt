58f3abeefe4c6d7c172eed32fff8846657084100
johnny3young <youngerblack@gmail.com>
2019-08-06 10:06:42 -0500

    Begin Navigation and quiz

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -18,7 +19,16 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        //Inflate the fragment for to be show in activity
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+
+        //Active button "play"
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment2_to_gameFragment) // or it.findNavController().navigate(R.id.action_titleFragment2_to_gameFragment)
+            // or
+            //binding.playButton.setOnClickListener (Navigation.createNavigateOnclickListener(R.d.action_titleFragment2_to_gameFragment)
+        }
+
         return binding.root
     }
 
